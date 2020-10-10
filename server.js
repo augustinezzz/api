332e0f4575c20992d6a4593ec1ecd87289990fd3
augustinezzz <athom236@uncc.edu>
2020-10-10 05:11:32 +0000

    api

--- server.js
+++ server.js
@@ -136,45 +136,7 @@ app.get('/customer', function (req,res){
 });
 
 
-/**
- * @swagger
- * /customer:
- *     delete:
- *       description: Return all agents
- *       produces:
- *           - application/json
- *       responses:
- *           200:
- *               description: Object containing agents contaitng array
- * 
- */
 
-
-app.delete('/customer/', function (req,res){
-  pool.getConnection()
-.then(conn => {
-
-conn.query("SELECT * FROM customer WHERE CUST_NAME ='req.body.name' ")
-  .then((rows) => {
-    console.log(rows);
-     res.json(rows);
-console.log("Displaying Records from the database filtered by country India")
-
-  })
-  .then((res) => {
-    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-    conn.end();
-  })
-  .catch(err => {
-    //handle error
-    console.log(err);
-    conn.end();
-  })
-
-}).catch(err => {
-//not connected
-})
-});
 /**
  * @swagger
  * /order:
@@ -215,52 +177,50 @@ app.get('/order', function (req,res){
 
 //post request root foods 
 
+
 /**
  * @swagger
  * definitions:
- *   Foods:
+ *   Company:
  *     properties:
- *       itemid:
- *         type: string
- *       itemname:
- *         type: string
- *       itemunit:
- *         type: string
  *       companyid:
  *         type: string
+ *       companyname:
+ *         type: string
+ *       companycity:
+ *         type: string
  */
+
 /**
  * @swagger
- * /foods:
+ * /company:
  *    post:
- *      description: add record to Foods table
+ *      description: add record to company table
  *      produces:
  *          - application/json
  *      responses:
  *          200:
- *              description: Added data to Foods table
+ *              description: Added data to company table
  *          500:
  *              description: Data already exists
  *      parameters:
- *          - name: Foods
+ *          - name: Company
  *            description: company object
  *            in: body
  *            required: true
  *            schema:
- *              $ref: '#/definitions/Foods'
+ *              $ref: '#/definitions/Company'
  *
  *
  */
-app.post("/foods",urlencodedParser,[
+app.post("/company",urlencodedParser,[
 
-  check('item_id').isNumeric()
-  .withMessage('id be a number').isLength({max:6}).withMessage("Max 5 numbers allowed"),
-  check('item_name').isAlpha().trim().escape()
-  .withMessage('Name should have Alphabets').isLength({max:25}).withMessage("Max 20 characters"),
-  check('item_unit').isAlpha().trim().escape()
-  .withMessage('Should be Alphabets').isLength({max:5}).withMessage("Max 5 characters"),
-  check('company_id').isAlpha().trim().escape()
-  .withMessage('Company ID should be only Alphabets').isLength({max:5}).withMessage("Compay ID should be max 5 ch")
+  check('companyid').isNumeric()
+  .withMessage('Company ID should be numbers').isLength({max:6}).withMessage("Maximum of 6 numbers"),
+  check('companyname').isAlpha()
+  .withMessage('Company Name should be Alphabets').isLength({max:25}).withMessage("Maximum of 25 characters"),
+  check('companycity').isAlpha()
+  .withMessage('City Name should be Alphabets').isLength({max:25}).withMessage("Maximum of 25 characters"),
 
 ],async(req, res) => {
 
@@ -270,25 +230,25 @@ app.post("/foods",urlencodedParser,[
           return res.status(422).json({ errors: errors.array() })
 
   }else{ 
-  if(req.body==null || req.body.itemname==null || req.body.itemunit==null
+  if(req.body==null || req.body.companyname==null || req.body.companycity==null
     || req.body.companyid==null){
      res.header("Content-Type", "application/json");
      res.status(400);
-     res.send("Body is Invalid");
+     res.send("INvalid Body ");
      return;
     }
   pool
     .getConnection()
     .then((conn) => {
-      conn.query("SELECT * FROM foods where ITEM_ID=?",[req.body.itemid]).then((row)=>{
+      conn.query("SELECT * FROM commpany where COMPANY_ID=?",[req.body.companyid]).then((row)=>{
         if(row.length>0){
           res.header("Content-Type", "application/json");
           res.status(500).send({error:"Data is already present"});
           conn.close();
           return;
         }
-        conn.query("INSERT INTO foods VALUE (?,?,?,?)",
-          [req.body.itemid,req.body.itemname, req.body.itemunit,req.body.companyid])
+        conn.query("INSERT INTO company VALUE (?,?,?)",
+          [req.body.companyid,req.body.companyname,req.body.companycity])
             .then((data) => {
               res.header("Content-Type", "application/json");
               res.status(200);
@@ -303,7 +263,256 @@ app.post("/foods",urlencodedParser,[
   }
 });
 
+//
+//
+// Delete Request
+//
+//
+//
+
+/**
+ * @swagger
+ * /company/{company_id}:
+ *    delete:
+ *      description: delete record from table
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: deleted data from company table
+ *          500:
+ *              description: Data already exists
+ *      parameters:
+ *          - name: company_id
+ *            description: company object
+ *            in: path
+ *            required: true
+ *
+ *
+ */
+app.delete('/company/:company_id',[
+  check('companyid').isNumeric()
+ .withMessage('Company ID should be numbers').isLength({max:6}).withMessage("Maximum of 6 numbers")  
+],(req, res)=>{
+ var errors= validationResult(req);
  
+ if (!errors.isEmpty()) {
+         return res.status(422).json({ errors: errors.array() })
+       
+ }else{
+   console.log(req.params)
+   if(req.params==null ){
+     res.header("Content-Type", "application/json");
+     res.status(400);
+     res.send("Enter a valid data ");
+   }
+ pool.getConnection()
+ .then(con =>{
+
+     con.query("SELECT * FROM company where COMPANY_ID=?",[req.params.company_id]).then((row)=>{
+         if(row.length==0){
+           res.header("Content-Type", "application/json");
+           res.status(500).send({error:"Data does not exist"});
+           con.close();
+           return;
+         }
+         con.query("DELETE from company WHERE COMPANY_ID='"+[req.params.company_id]+"'")
+         .then(()=>{
+             
+             res.send("Successfully Deleted");
+             con.end();
+         })
+     })
+        .catch(err =>{
+             // print the error
+             console.log(err);
+             // close the connection
+             con.end();
+         });
+ }).catch(err=>{
+         console.log(err);
+ });
+}
+});
+
+//
+//
+// Patch Request
+//
+//
+//
+
+
+
+
+
+
+ /**
+ * @swagger
+ * /company:
+ *    patch:
+ *      description: add record to company table
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: Patched data to company table
+ *      parameters:
+ *          - name: company
+ *            description: company object
+ *            in: body
+ *            required: true
+ *            schema:
+ *              $ref: '#/definitions/company'
+ *
+ *
+ */
+app.patch('/company',[
+  check('companyid').isNumeric()
+  .withMessage('Company ID should be numbers').isLength({max:6}).withMessage("Maximum of 6 numbers"),
+  check('companyname').isAlpha()
+  .withMessage('Company Name should be Alphabets').isLength({max:25}).withMessage("Maximum of 25 characters"),
+  check('companycity').isAlpha()
+  .withMessage('City Name should be Alphabets').isLength({max:25}).withMessage("Maximum of 25 characters"),
+  
+] ,(req, res)=>{
+  var errors= validationResult(req);
+  
+  if (!errors.isEmpty()) {
+          return res.status(422).json({ errors: errors.array() })
+        
+  }else{
+
+  pool.getConnection()
+  .then(con =>{
+          
+           con.query("SELECT * FROM company where COMPANY_ID=?",[req.body.companyid]).then((data)=>{
+             console.log(data[0]);
+             const arr=data[0];
+
+             if(req.body.companyname!=null){
+                 arr.COMPANY_NAME=req.body.companyname
+              }
+             if(req.body.companycity!=null){
+              arr.COMPANY_CITY=req.body.companycity
+              }
+             
+              if (data.length == 0){
+                res.status(500).send({error:"No such company exist"});
+                con.close();
+                return;
+              }
+              con.query("UPDATE company SET COMPANY_NAME=?, COMPANY_CITY=? WHERE COMPANY_ID=?",
+              [arr.COMPANY_NAME, arr.COMPANY_CITY, arr.COMPANY_ID])
+                .then(()=>{
+                 
+                res.send("Successfully Updated");
+                con.end();
+              })
+            
+         })
+         .catch(err =>{
+              
+              console.log(err);
+              
+              con.end();
+          });
+  }).catch(err=>{
+          console.log(err);
+  });
+}
+}); 
+
+
+//
+//
+// Put request
+//
+//
+//
+//
+
+
+  /**
+ * @swagger
+ * /company/put:
+ *    put:
+ *      description: add record to company table
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description:  data put to company table
+ *          500:
+ *              description: Data already exists
+ *      parameters:
+ *          - name: Foods
+ *            description: company object
+ *            in: body
+ *            required: true
+ *            schema:
+ *              $ref: '#/definitions/Company'
+ *
+ *
+ */
+app.put("/company",[
+
+  check('companyid').isNumeric()
+  .withMessage('Company ID should be numbers').isLength({max:6}).withMessage("Maximum of 6 numbers"),
+  check('companyname').isAlpha()
+  .withMessage('Company Name should be Alphabets').isLength({max:25}).withMessage("Maximum of 25 characters"),
+  check('companycity').isAlpha()
+  .withMessage('City Name should be Alphabets').isLength({max:25}).withMessage("Maximum of 25 characters"),
+
+] ,(req, res) => {
+  var errors= validationResult(req);
+  
+  if (!errors.isEmpty()) {
+          return res.status(422).json({ errors: errors.array() })
+        
+  }else{
+
+  console.log(req.body);
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn.query("SELECT * FROM company where COMPANY_ID=?",[req.body.companyid]).then((row)=>{
+        if(row.length==0){
+          res.header("Content-Type", "application/json");
+          conn.query("INSERT INTO company VALUE (?,?,?)",
+          [req.body.companyid,req.body.companyname,req.body.companycity])
+            .then((data) => {
+              res.header("Content-Type", "application/json");
+              res.status(200);
+              res.send(data);
+              conn.close();
+            })
+           
+          return;
+        }
+        conn.query("UPDATE company SET COMPANY_ID=?, COMPANY_NAME=?, COMPANY_CITY=? WHERE COMPANY_ID=?",
+          [req.body.companyid, req.body.companyname,req.body.companycity])
+            .then((data) => {
+              res.header("Content-Type", "application/json");
+              res.status(200);
+              res.send(data);
+              conn.close();
+            })
+            .catch((err) => {
+              console.log(err);
+              conn.end();
+            });
+
+      })
+    .catch((err) => {
+      console.log(err);
+      conn.end();
+    });
+  });
+}
+});
+
+
 app.listen(port, () => {
         console.log (`Listening to port ${port}!`)
 });
